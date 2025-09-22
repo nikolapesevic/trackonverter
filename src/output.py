4da6eb34f4faad0775bad6261b1187917165546a
triankl3 <25014845+triankl3@users.noreply.github.com>
2025-09-22 23:18:54 +0200

    Improved output

--- src/output.py
+++ src/output.py
@@ -1,18 +1,20 @@
 from rich import print as rprint
 from rich.prompt import Prompt
+from rich.console import Console
+
+console = Console()
 
 def error(message: str):
-	rprint(f"❌ [bold red]Error: {message} [/bold red]")
+	rprint(f"❌[bold red] Error: {message} [/bold red]")
 
 def warn(message: str):
 	rprint(f"⚠️ [bold yellow] {message} [/bold yellow]")
 
 def success(message: str):
-	rprint(f"✅ [bold green] {message} [/bold green]")
+	rprint(f"✅[bold green] {message} [/bold green]")
 
 def info(message: str):
-	rprint(f"ℹ️ [bold blue] {message} [/bold blue]")
+	rprint(f"ℹ️[bold blue]  {message} [/bold blue]") # Extra space due to crappy emoji width
 
 def prompt(message: str, default: str) -> str:
-	rprint(f"💬 {message} [bold blue][Default: {default}][/bold blue]: ")
-	return input().strip() or default
+	return Prompt.get_input(console, prompt=f"💬 {message} [bold blue][Default: {default}][/bold blue]: ", password=False).strip() or default
