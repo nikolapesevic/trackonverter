e1fe064bf5f1dd475373cf8d9e92e2e67ab77c20
triankl3 <25014845+triankl3@users.noreply.github.com>
2025-10-15 23:12:03 +0200

    Get basic flet setup to work

--- src/main.py
+++ src/main.py
@@ -1,59 +1,85 @@
 #!/usr/bin/env python3
 
-import sys
-import os
-import subprocess
-import output
-import ffmpeg
-from pathlib import Path
-from track_converter import TrackConverter
+# import sys
+# import os
+# import subprocess
+# import output
+# import ffmpeg
+# from pathlib import Path
+# from track_converter import TrackConverter
 
-DEFAULT_FOLDER = "tracks"
+# DEFAULT_FOLDER = "tracks"
 
-# Allow running terminal from macOS app bundle with the right cwd
-def is_app_bundle():
-	return getattr(sys, 'frozen', False) and '.app/Contents/MacOS' in sys.executable
+# # Allow running terminal from macOS app bundle with the right cwd
+# def is_app_bundle():
+# 	return getattr(sys, 'frozen', False) and '.app/Contents/MacOS' in sys.executable
 
-def launch_in_terminal():
-	script_path = sys.executable
-	app_parent_dir = str(Path(script_path).parents[3])
-	applescript = f'''
-	tell application "Terminal"
-		activate
-		do script "clear && cd '{app_parent_dir}' && '{script_path}'"
-	end tell
-	'''
-	subprocess.run(['osascript', '-e', applescript])
+# def launch_in_terminal():
+# 	script_path = sys.executable
+# 	app_parent_dir = str(Path(script_path).parents[3])
+# 	applescript = f'''
+# 	tell application "Terminal"
+# 		activate
+# 		do script "clear && cd '{app_parent_dir}' && '{script_path}'"
+# 	end tell
+# 	'''
+# 	subprocess.run(['osascript', '-e', applescript])
 
-def main():
-	# If running from app bundle and no terminal is attached, launch in Terminal
-	if is_app_bundle() and not sys.stdin.isatty() and '--terminal' not in sys.argv:
-		launch_in_terminal()
-		return
+# def main():
+# 	# If running from app bundle and no terminal is attached, launch in Terminal
+# 	if is_app_bundle() and not sys.stdin.isatty() and '--terminal' not in sys.argv:
+# 		launch_in_terminal()
+# 		return
 		
-	if ffmpeg.ensure():
-		try: 
-			args = sys.argv
-			user_path = None
-			if len(args) == 2:
-				user_path = args[1]
-			else:
-				user_path = output.prompt("Drag & drop a folder or type a path and press ➡️  Enter.", DEFAULT_FOLDER)
-				if os.path.isdir(user_path):
-					output.info("Using input folder: " + user_path)
-				else:
-					output.error(f"No input folder specified and no '{DEFAULT_FOLDER}' folder found in current directory")
+# 	if ffmpeg.ensure():
+# 		try: 
+# 			args = sys.argv
+# 			user_path = None
+# 			if len(args) == 2:
+# 				user_path = args[1]
+# 			else:
+# 				user_path = output.prompt("Drag & drop a folder or type a path and press ➡️  Enter.", DEFAULT_FOLDER)
+# 				if os.path.isdir(user_path):
+# 					output.info("Using input folder: " + user_path)
+# 				else:
+# 					output.error(f"No input folder specified and no '{DEFAULT_FOLDER}' folder found in current directory")
 
-			track_converter = TrackConverter(user_path)
-			track_converter.additional_converters = ["aiff"]
-			track_converter.run()
-		except KeyboardInterrupt:
-			output.info("Interupted by user")
-		except Exception as e:
-			output.error(f"Unexpected error - {e}")
+# 			track_converter = TrackConverter(user_path)
+# 			track_converter.additional_converters = ["aiff"]
+# 			track_converter.run()
+# 		except KeyboardInterrupt:
+# 			output.info("Interupted by user")
+# 		except Exception as e:
+# 			output.error(f"Unexpected error - {e}")
 
-	output.wait_for_key()
+# 	output.wait_for_key()
 
 
-if __name__ == "__main__":
-	main()
+# if __name__ == "__main__":
+# 	main()
+
+import flet as ft
+
+def main(page: ft.Page):
+    counter = ft.Text("0", size=16, data=0)
+
+    def increment_click(e):
+        counter.data += 1
+        counter.value = str(counter.data)
+        counter.update()
+
+    page.floating_action_button = ft.FloatingActionButton(
+        icon=ft.Icons.ADD, on_click=increment_click
+    )
+    page.add(
+        ft.SafeArea(
+            ft.Container(
+                counter,
+                alignment=ft.alignment.center,
+            ),
+            expand=True,
+        )
+    )
+
+
+ft.app(main)
